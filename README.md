5cefae36af67cc00a87a64f7a597cfc30c39fda7
Maxim Sinyukov <verero1173@yandex.ru>
2021-05-09 23:34:14 +0300

    feat: инициализировать проект и убрать шаблоны

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sw-info
+
+<p>
+  <img alt="Status" src="https://img.shields.io/badge/status-unreleased-orange" >
+  <img alt="Author" src="https://img.shields.io/badge/author-MaximSinyukov-blue" />
+</p>
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
+  -ms-text-size-adjust: 100%;
+  -moz-text-size-adjust: 100%;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
