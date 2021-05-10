ffe6bd2095bd6f7688d33766324dd54b64601bdf
Maxim Sinyukov <verero1173@yandex.ru>
2021-05-11 01:00:20 +0300

    refactor: вынести логику запроса нового раздела в отдельный компонент

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <select v-model="request">
-      <option value="planets">Планеты</option>
-      <option value="films">Фильмы</option>
-      <option value="starships">Звездолёты</option>
-      <option value="people">Люди</option>
-    </select>
+    <Navigation />
     <h2>Я видел</h2>
     <ul class="list">
       <Loader v-if="getLoading" />
@@ -23,25 +18,16 @@
 import { mapGetters, mapActions } from 'vuex'
 import ListItem from '@/components/ListItem'
 import Loader from '@/components/Loader'
+import Navigation from '@/components/Navigation'
 
 export default {
-  data() {
-    return {
-      request: 'planets'
-    }
-  },
   computed: mapGetters(['allItems', 'getLoading']),
   methods: mapActions(['fetchItems']),
   async mounted() {
     this.fetchItems();
   },
-  watch: {
-    request(value) {
-      this.fetchItems(value);
-    }
-  },
   components: {
-    ListItem, Loader
+    ListItem, Loader, Navigation
   }
 }
 </script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="nav">
+    <select class="nav__select" v-model="request">
+      <option value="planets">Планеты</option>
+      <option value="films">Фильмы</option>
+      <option value="starships">Звездолёты</option>
+      <option value="people">Люди</option>
+    </select>
+  </nav>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  data() {
+    return {
+      request: 'planets'
+    }
+  },
+  methods: mapActions(['fetchItems']),
+  watch: {
+    request(value) {
+      this.fetchItems(value);
+    }
+  },
+}
+</script>
+
+<style>
+.nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 20px auto 20px;
+}
+
+.nav__select {
+  border: 1px solid goldenrod;
+  font-size: 18px;
+  line-height: 20px;
+  outline: none;
+}
+
+.nav__select:hover {
+  cursor: pointer;
+}
+</style>
